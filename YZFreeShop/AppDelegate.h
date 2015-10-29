cd8c66f99f76cf6337bb5c903afbb59bda042350
lunarboat <lunarboat@outlook.com>
2015-10-29 20:18:28 +0800

    Initial Commit

---
+++ YZFreeShop/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  YZFreeShop
+//
+//  Created by lunarboat on 15/10/29.
+//  Copyright © 2015年 lunarboat. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ YZFreeShop/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  YZFreeShop
+//
+//  Created by lunarboat on 15/10/29.
+//  Copyright © 2015年 lunarboat. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
