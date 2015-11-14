6fc1216f86d75fd0a132921a083438b0398a1dc9
lunarboat <lunarboat@outlook.com>
2015-11-14 20:15:32 +0800

    cache
    增加了网络缓存

--- YZFreeShop/AppDelegate.m
+++ YZFreeShop/AppDelegate.m
@@ -23,6 +23,11 @@
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     _lastStatus = AFNetworkReachabilityStatusUnknown;
     [self networkReachability];
+    
+    //设置网络缓存NSURLCache
+    NSURLCache *URLCache = [[NSURLCache alloc]initWithMemoryCapacity:4 *1024 * 1024 diskCapacity:20 * 1024 * 1024 diskPath:nil];
+    [NSURLCache setSharedURLCache:URLCache];
+    
     return YES;
 }
 
